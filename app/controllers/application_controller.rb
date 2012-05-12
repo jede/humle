810427cdc05b6ed6741e65a9b28888ce3574fe9f
Johannes Edelstam <johannes@edelst.am>
2012-05-12 16:30:50 +0200

    position fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   helper_method :current_position
+  helper_method :locked_current_position?
   protect_from_forgery
 
   def current_position
     session[:current_position] || {latitude: '59.317546', longitude: '18.064098'}
+  end
+
+  def locked_current_position?
+    session.has_key?(:current_position)
   end
 end
