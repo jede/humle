c50e4175c2fc58400405063e1486f2281f177c9d
Johannes Edelstam <johannes@edelst.am>
2012-05-12 09:09:10 +0200

    Added rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Humle::Application.config.secret_token = '48162c211349091f5b0fcb3879d560fe8f477fcc02978ef7fcf0f31c96a06c1c5b87bae45f70811b074e7710eacb2170c98224af75cc220bd95a32315b96cbe9'
