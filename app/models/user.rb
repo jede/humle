3517657f4f187935ad4bd703bf422e6225df522b
Johannes Edelstam <johannes@edelst.am>
2012-05-12 12:02:21 +0200

    Add fb callbacks

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,4 +39,22 @@ class User
 
   ## Token authenticatable
   # field :authentication_token, :type => String
+  #
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"]
+      end
+    end
+  end
+
+  def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
+    data = access_token.extra.raw_info
+    if user = self.find_by_email(data.email)
+      user
+    else # Create a user with a stub password. 
+      self.create!(:email => data.email, :password => Devise.friendly_token[0,20]) 
+    end
+  end
 end
